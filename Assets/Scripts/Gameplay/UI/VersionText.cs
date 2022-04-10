6bafa453ae61b3dfb2b40c56988ed5379fe338b3
kremnev8 <kremnev8super@yandex.ru>
2022-04-10 22:01:21 +0300

    Add version text

---
+++ Assets/Scripts/Gameplay/UI/VersionText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class VersionText : MonoBehaviour
+    {
+        private TMP_Text label;
+
+        private void Awake()
+        {
+            label = GetComponent<TMP_Text>();
+            label.text = $"Alpha v{Application.version}";
+            
+        }
+    }
+}
