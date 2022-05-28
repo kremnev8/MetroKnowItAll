bfed34a0721e4383552c42b78037ce8844c92729
kremnev8 <kremnev8super@yandex.ru>
2022-05-28 15:30:49 +0300

    Refactor main menu code

--- Assets/Scripts/Gameplay/UI/UIMainMenu.cs
+++ Assets/Scripts/Gameplay/UI/UIMainMenu.cs
@@ -5,33 +5,17 @@ namespace Gameplay.UI
     public class UIMainMenu : MonoBehaviour
     {
         public GameObject gameModeScreen;
-        public GameObject questionTypeScreen;
         public GameObject crossfade;
 
         private int selectedGameMode;
-        private int selectedQuestionType;
-
-        public void SelectGameModeAndLoad(int gameMode)
-        {
-            selectedGameMode = gameMode;
-            gameModeScreen.SetActive(false);
-            questionTypeScreen.SetActive(false);
-            StartLoad();
-        }
 
         public void SelectGameMode(int gameMode)
         {
             selectedGameMode = gameMode;
             gameModeScreen.SetActive(false);
-            questionTypeScreen.SetActive(true);
-        }
-
-        public void SelectQuestionMode(int questionMode)
-        {
-            selectedQuestionType = questionMode;
-            questionTypeScreen.SetActive(false);
             StartLoad();
         }
+        
 
         private void StartLoad()
         {
@@ -40,7 +24,7 @@ namespace Gameplay.UI
                 if (crossfade != null)
                     crossfade.SetActive(true);
 
-                SceneTransitionManager.instance.StartGame(selectedGameMode, selectedQuestionType);
+                SceneTransitionManager.instance.StartGame(selectedGameMode);
             }
         }
     }
